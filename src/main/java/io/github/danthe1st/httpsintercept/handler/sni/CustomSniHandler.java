0aba5bf598ea40430ac6945d0ef1eaaf5d893ef3
danthe1st <daniel@wwwmaster.at>
2024-03-11 18:21:39 +0100

    handle case when no hostname present

--- src/main/java/io/github/danthe1st/httpsintercept/handler/sni/CustomSniHandler.java
+++ src/main/java/io/github/danthe1st/httpsintercept/handler/sni/CustomSniHandler.java
@@ -13,6 +13,7 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.handler.ssl.SniHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.util.Mapping;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,30 +32,36 @@ public class CustomSniHandler extends SniHandler {
 	}
 	
 	@Override
-	protected void replaceHandler(ChannelHandlerContext channelHandlerContext, String hostname, SslContext sslContext) throws Exception {
+	protected void replaceHandler(ChannelHandlerContext channelHandlerContext, @Nullable String hostname, SslContext sslContext) throws Exception {
 		ChannelPipeline pipeline = channelHandlerContext.pipeline();
-		// TODO no hostname
-		if(ignoredHosts.allMatches(hostname).hasNext()){
+		if(hostname == null){
+			LOG.error("no hostname present - aborting connection");
+			channelHandlerContext.close();
+		}else if(ignoredHosts.allMatches(hostname).hasNext()){
 			LOG.info("skipping hostname {}", hostname);
 			
-			boolean foundThis = false;
-			
-			for(Iterator<Map.Entry<String, ChannelHandler>> it = pipeline.iterator(); it.hasNext();){
-				ChannelHandler handler = it.next().getValue();
-				if(foundThis){
-					it.remove();
-				}
-				if(handler == this){
-					foundThis = true;
-				}
-			}
-			
-			if(!foundThis){
-				throw new IllegalStateException("cannot find self handler in pipeline");
-			}
-			pipeline.replace(this, "forwardNoProcess", new RawForwardIncomingRequestHandler(hostname, clientBootstrapTemplate));
+			replaceWithRawForward(hostname, pipeline);
 		}else{
 			super.replaceHandler(channelHandlerContext, hostname, sslContext);
 		}
+	}
+
+	private void replaceWithRawForward(String hostname, ChannelPipeline pipeline) {
+		boolean foundThis = false;
+		
+		for(Iterator<Map.Entry<String, ChannelHandler>> it = pipeline.iterator(); it.hasNext();){
+			ChannelHandler handler = it.next().getValue();
+			if(foundThis){
+				it.remove();
+			}
+			if(handler == this){
+				foundThis = true;
+			}
+		}
+		
+		if(!foundThis){
+			throw new IllegalStateException("cannot find self handler in pipeline");
+		}
+		pipeline.replace(this, "forwardNoProcess", new RawForwardIncomingRequestHandler(hostname, clientBootstrapTemplate));
 	}
 }
